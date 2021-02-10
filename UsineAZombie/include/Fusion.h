a5e77ad939f2cc19ac565e1409ebefe58b633e27
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-02-10 12:52:29 +0100

    rajout fusion

---
+++ UsineAZombie/include/Fusion.h
@@ -0,0 +1,16 @@
+#ifndef FUSION_H
+#define FUSION_H
+
+#include "Zombie.h"
+
+
+class Fusion : public Zombie
+{
+    public :
+        Fusion(int pv, int atk, string n);
+        ~Fusion();
+        void absorption(Zombie* cible);
+
+};
+
+#endif
---
+++ UsineAZombie/src/Fusion.cpp
@@ -0,0 +1,17 @@
+#include "Fusion.h"
+
+Fusion::Fusion(int p, int a, string n)
+{
+   setPv(p);
+   setAtk(a);
+   setName(n);
+}
+
+Fusion::~Fusion()
+{
+
+}
+
+void Fusion::absorption(Zombie* cible){
+
+    }
