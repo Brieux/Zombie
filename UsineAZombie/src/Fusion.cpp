a78562e0f9b80d1bf0a84e166b2979bf11a9d0fc
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-02-10 13:00:45 +0100

    Fini

--- UsineAZombie/src/Fusion.cpp
+++ UsineAZombie/src/Fusion.cpp
@@ -12,10 +12,17 @@ Fusion::~Fusion()
 
 }
 
-void Fusion::absorption(Zombie* cible){
+void Fusion::absorption(Zombie* cible, string cible){
     if ((getPv() > cible->getPv() )|| (getAtk() > cible ->getAtk())){
         setPv(getPv() + cible->getPv());
         setAtk(getAtk() + cible->getAtk());
+        if (cible =="tank"){
+            setPv(getPv()+cible->getArm());
+        }
+        if (cible =="Bomber"){
+            cible->~Zombie();
+            ~Zombie;
+        }
         cible->~Zombie();
     }
     if (getPv() > 500 && getAtk() > 200){
